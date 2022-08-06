27c68977771572fcd25fb4236d31d4381fd9d4c7
Smitty Werben <smittywerbenjj.1@gmail.com>
2022-08-06 07:22:59 +0200

    added project files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Blender pipeline tools
+
+Tools to faciliate working with other software
+
+
+- Splitting a Mesh by its material and renaming each splitted mesh to the name of the material
+
+- Batch exporting all child-meshes of a Armature into a seperate file
---
+++ scripts/Seperate by Materials.py
@@ -0,0 +1,39 @@
+import bpy
+
+
+def getMeshObjectsInView():
+    return [x for x in bpy.context.view_layer.objects if x.type == "MESH"]
+
+
+print("SPlitting...")
+active_obj = bpy.context.view_layer.objects.active
+
+if active_obj == None:
+    raise Exception("YOU HAVE TO SELECT A MESH OBJECT IN EDIT MODE")
+if not (active_obj.type == "MESH"):
+    raise Exception("YOU HAVE TO SELECT A MESH OBJECT IN EDIT MODE")
+
+
+all_objects = getMeshObjectsInView()
+bpy.ops.object.mode_set(mode="EDIT")
+bpy.ops.mesh.separate(type="MATERIAL")
+new_all_objects = getMeshObjectsInView()
+
+for old_obj in all_objects:
+    if old_obj in new_all_objects:
+        new_all_objects.remove(old_obj)
+new_all_objects.append(bpy.context.view_layer.objects.active)
+
+# rename objects
+for obj in new_all_objects:
+    material = obj.material_slots[0].material
+    try:
+        dashindex = material.name.index("-")
+    except ValueError:
+        dashindex = -1
+    if dashindex == -1:
+        obj.name = material.name
+    else:
+        obj.name = material.name[:dashindex]
+
+bpy.ops.object.mode_set(mode="OBJECT")
